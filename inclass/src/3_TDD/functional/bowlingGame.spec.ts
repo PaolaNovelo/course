80e225dedeb6aa4c1436b9ffd069a8aac3649ed7
Paola Novelo <pau.novelo0609@gmail.com>
2024-02-07 11:40:39 -0600

    ejercicios TDD

--- inclass/src/3_TDD/functional/bowlingGame.spec.ts
+++ inclass/src/3_TDD/functional/bowlingGame.spec.ts
@@ -1,8 +1,54 @@
+import { createBowlingGame } from "./bowlingGame";
 
-describe('Test bowling game', () => {
+describe('Functional Tests for Bowling Game', () => {
+  let game = createBowlingGame();
 
-  it('dummy test', () => {
-    expect(1).toBe(1)
-  })
+  beforeEach(() => {
+    game = game.resetState();
+  });
 
+  it('should get a game with 0 points', () => {
+    game = rollManyTimes(20, 0, game);
+    expect(game.getFinalScore()).toBe(0);
+  });
+
+  it('should get a game with all ones', () => {
+    game = rollManyTimes(20, 1, game);
+    expect(game.getFinalScore()).toBe(20);
+  });
+
+  it('should get a game with a spare', () => {
+    game = rollSpare(game);
+    game = game.rollBall(3);
+
+    game = rollManyTimes(17, 0, game);
+    expect(game.getFinalScore()).toBe(16);
+  });
+
+  it('should get a game with a strike', () => {
+    game = game.rollBall(10);
+    game = game.rollBall(3);
+    game = game.rollBall(3);
+
+    game = rollManyTimes(16, 0, game);
+    expect(game.getFinalScore()).toBe(22);
+  });
+
+  it('should be a perfect game', () => {
+    game = rollManyTimes(12, 10, game);
+    expect(game.getFinalScore()).toBe(300);
+  });
 });
+
+function rollSpare(game: any) {
+  game = game.rollBall(5);
+  game = game.rollBall(5);
+  return game;
+}
+
+function rollManyTimes(rolls: number, pins: number, game: any) {
+  for (let i = 0; i < rolls; i++) {
+    game = game.rollBall(pins);
+  }
+  return game;
+}
--- inclass/src/3_TDD/functional/bowlingGame.ts
+++ inclass/src/3_TDD/functional/bowlingGame.ts
@@ -1,22 +1,52 @@
-/* Remember that all your functions have to respect the functional programming rules:
- *  - 🔥 Pure functions and side effects 
- *  - 🔥 First class functions & higher order functions
- *  - 🔥 Immutability ⚠️
- *  - 🔥 Composition 
- *
- * See more info here: 
- * https://www.learningjournal.guru/courses/scala/scala-programming-foundation/elements-of-functional-programming-1/
- * 
- * I'm putting a warning in Immutability to remember you that every time you try
- * to change the state var... just be carefull...⚠️
- */
+type State = { 
+  rolls: number[] 
+}
 
-type State = { /* put params if needed */ }
-const INITIAL_STATE: State = {}
+const INITIAL_STATE: State = {
+  rolls: []
+}
 
 export const createBowlingGame = (state: State = INITIAL_STATE) => Object.freeze({
-  //myFunction: () => callInnerModuleScopeFunction()
-  //myOhterFunction: (...) => myOtherFunction(...)
+  rollBall: (pins: number) => rollBall(pins, state),
+  getFinalScore: () => getFinalScore(state),
+  resetState: () => resetState()
 })
 
-//const myOtherFunction = (...) => {...}
+const rollBall = (pins: number, state: State) => {
+  const rolls = [...state.rolls, pins];
+  return createBowlingGame({ rolls });
+}
+
+const getFinalScore = ({ rolls }: State) => {
+  let total = 0;
+  let throwNumber = 0;
+  const FRAMES = 10;
+
+  for (let frame = 0; frame < FRAMES; frame++) {
+    if (isStrike(rolls, throwNumber)) {
+      total += 10 + getStrikeBonus(rolls, throwNumber);
+      throwNumber++;
+    } else if (isSpare(rolls, throwNumber)) {
+      total += 10 + getSpareBonus(rolls, throwNumber);
+      throwNumber += 2;
+    } else {
+      total += getFrameScore(rolls, throwNumber);
+      throwNumber += 2;
+    }
+  }
+  return total;
+}
+
+const isStrike = (rolls: number[], throwNumber: number) => rolls[throwNumber] === 10;
+
+const isSpare = (rolls: number[], throwNumber: number) => rolls[throwNumber] + rolls[throwNumber + 1] === 10;
+
+const getStrikeBonus = (rolls: number[], throwNumber: number) => rolls[throwNumber + 1] + rolls[throwNumber + 2];
+
+const getSpareBonus = (rolls: number[], throwNumber: number) => rolls[throwNumber + 2];
+
+const getFrameScore = (rolls: number[], throwNumber: number) => rolls[throwNumber] + rolls[throwNumber + 1];
+
+const resetState = () => createBowlingGame();
+
+export default createBowlingGame;
